81d6accc74909229ba17e0e9d3e710668e7ebab8
pranit <pranitsawant01@gmail.com>
2018-04-08 11:01:47 +0530

    MISC fixes

--- app/train_model.py
+++ app/train_model.py
@@ -1,5 +1,5 @@
 # Todo: Load the data set
 
-# Todo: Create X and Y array
+# Todo: Create X(Features) and Y(Value to predict) array
 
 # Todo: Split the data set in a training set (70%) and a test set (30%)
